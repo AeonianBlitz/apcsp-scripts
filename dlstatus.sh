0c18ba50c7ff829a4d791cbc7b6e52f16c8b5d80
Daniel Choi <ChoiH9054@my.sduhsd.net>
2020-12-19 05:41:15 +0000

    Pushing the final Bash Alive project

--- dlstatus.sh
+++ dlstatus.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-echo "hello world"
+DROPLETS="dl01 dl02 dl03 dl04 dl05 dl06 dl07 dl08 dl09 dl10 dl11";
+
+if [ $# -gt 0 ] ; then
+	for arg in $@ ; do
+	ping -c 1 $arg > /dev/null;
+	STATUS=$?;
+		if [ $STATUS -eq 0 ] ; then
+			echo $arg : alive
+		else
+			echo $arg : dead
+		fi
+	done
+else
+	for droplet in $DROPLETS ; do
+	ping -c 1 $droplet > /dev/null;
+	STATUS=$?;
+		if [ $STATUS -eq 0 ] ; then
+			echo $droplet : alive
+		else
+			echo $droplet : dead
+		fi
+	done
+fi
+
+exit 0
